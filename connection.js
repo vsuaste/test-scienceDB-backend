b4d4175f8cfcd6392fe566274b79e8e6699f6db2
vsuaste <suaste@cimat.mx>
2018-03-04 01:32:13 -0600

    basic sequelize

---
+++ connection.js
@@ -0,0 +1,47 @@
+Sequelize = require('sequelize');
+_ = require('lodash');
+ Faker = require('faker');
+
+const Conn = new Sequelize(
+  'tutorial',
+  'tutorial',
+  'tutorial',
+  {
+    dialect: 'postgres',
+    host: '127.0.0.1'
+  }
+);
+
+const Person = Conn.define('person',{
+  firstName:{
+    type: Sequelize.STRING,
+    allowNull:false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNul:false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  }
+});
+
+//Generate fake data for table
+Conn.sync({force: true}).then(()=> {
+    _.times(15, ()=>{
+      return Person.create({
+        firstName: Faker.name.firstName(),
+        lastName: Faker.name.lastName(),
+        email: Faker.internet.email()
+      }).then( person => {
+        return person;
+        })
+      });
+    });
+
+
+module.exports.connection = Conn;
